4ce78d8f1dff5adfdf64d38d6a30e64642322cc1
Rupani Harsh Vallabhbhai <Harsh.Rupani1@ibm.com>
2024-01-03 21:10:44 +0530

    01-middlewares completed

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
@@ -12,16 +12,65 @@ const app = express();
 // clears every one second
 
 let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+// setInterval(() => {
+//   numberOfRequestsForUser = {};
+// }, 1000);
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.use((req, res, next) => {
+  const userId = req.headers["user-id"];
+  // console.log(userId);
+  // console.log(numberOfRequestsForUser);
+  // console.log(numberOfRequestsForUser[userId]);
+  // const val = numberOfRequestsForUser[userId] || 0;
+  // console.log(val);
+
+  // if (val >= 5) {
+  //   res.status(404).send({});
+  // }
+
+  // numberOfRequestsForUser[userId] = val + 1;
+
+  // console.log(numberOfRequestsForUser);
+
+  if (userId) {
+    const currentTime = Date.now();
+    const userRequests = numberOfRequestsForUser[userId];
+    console.log(userRequests);
+
+    if (!userRequests) {
+      console.log("if");
+      numberOfRequestsForUser[userId] = [
+        {
+          timestamp: currentTime,
+          // count: 1,
+        },
+      ];
+    } else {
+      let val = userRequests.filter((curr) => currentTime - curr.timestamp < 1000);
+
+      console.log("else");
+      console.log(val);
+      if (val.length >= 5) {
+        res.send(404);
+      }
+
+      numberOfRequestsForUser[userId].push({
+        timestamp: currentTime,
+        // count: userRequests.length + 1,
+      });
+    }
+    console.log(numberOfRequestsForUser);
+  }
+
+  next();
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
+});
+
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
 module.exports = app;
